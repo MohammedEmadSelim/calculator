b63ea4f4f3e86c2e290de96f4c3cbb82b23e7592
Mohammed <mohammedemad557@yahoo.com>
2025-05-27 17:45:04 +0300

    palindrome and comment

--- bin/calculator.dart
+++ bin/calculator.dart
@@ -1,9 +1,36 @@
+import 'dart:io';
+
 import 'package:args/args.dart';
 
 const String version = '0.0.1';
 
-
 void main(List<String> arguments) {
+ List names= ["ahmed","mohammed","khalid"];
 
+
+ // one line comment
+ /*
+ *
+ *
+ * multiple line comment
+ *
+ *
+ *
+ * */
+
+
+ print(isPalindrome('madam'));
+ /// this names OF NTI collection
+ for (var item in names){
+   print(item);
+ }
+}
+bool isPalindrome(String input){
+  for(int i =0;i<input.length;i++){
+    if(input[i] != input[input.length-1-i]){
+      return false;
+    }
+  }
+  return true;
 
 }
